5937d8c709a0f5ac12aa74e9ce18db7cffdac384
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 15:30:41 +0200

    Hotfix CSS Modules

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,46 +3,50 @@ import styles from './Form.module.scss';
 
 const Form = ({ submitFn }) => (
   <div className={styles.wrapper}>
-    <h2>Add new twitter account</h2>
+    <h2 className={styles.h2}>Add new twitter account</h2>
     <form
       className={styles.form}
       onSubmit={submitFn}
     >
       <div className={styles.item}>
         <input
+          className={styles.input}
           type="text"
           name="name"
           id="name" placeholder=" " maxLength="30"
         />
-        <label htmlFor="name">Name</label>
+        <label className={styles.label} htmlFor="name">Name</label>
         <div className={styles.itemBar}></div>
       </div>
       <div className={styles.item}>
         <input
+          className={styles.input}
           type="text"
           name="link"
           id="link" placeholder=" "
         />
-        <label htmlFor="link">Link</label>
+        <label className={styles.label} htmlFor="link">Link</label>
         <div className={styles.itemBar}></div>
       </div>
       <div className={styles.item}>
         <input
+          className={styles.input}
           type="text"
           name="image"
           id="image"
           placeholder=" "
         />
-        <label htmlFor="image">Image</label>
+        <label className={styles.label} htmlFor="image">Image</label>
         <div className={styles.itemBar}></div>
       </div>
       <div className={styles.item}>
         <textarea
+          className={styles.textarea}
           name="description"
           id="description"
           placeholder=" "
         />
-        <label htmlFor="description">Description</label>
+        <label className={styles.label} htmlFor="description">Description</label>
         <div className={styles.itemBar}></div>
       </div>
       <button
@@ -50,7 +54,7 @@ const Form = ({ submitFn }) => (
         type="submit"
       >
         add new item
-            </button>
+      </button>
     </form>
   </div>
 );
