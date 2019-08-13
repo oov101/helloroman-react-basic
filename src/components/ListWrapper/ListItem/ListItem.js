04d2911d03725c2b26ffc5c6b0534de59d67f28a
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 11:23:03 +0200

    Style ListItem component

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -4,10 +4,14 @@ import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => (
   <li className="listItem__wrapper">
-    <img src={danAbramovImage} alt="Dan Abramov" />
-    <h2>Dan Abramov</h2>
-    <p>Working on @reactjs. The demo guy.</p>
-    <button>visit twitter page</button>
+    <img className="listItem__image" src={danAbramovImage} alt="Dan Abramov" />
+    <div>
+      <h2 className="listItem__name">Dan Abramov</h2>
+      <p className="listItem__description">
+        Working on @reactjs. The demo guy.
+      </p>
+      <button className="listItem__button">visit twitter page</button>
+    </div>
   </li>
 );
 
