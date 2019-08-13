d0fdd8a3d2206a7bad4db3e9a7bd465db061805c
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 14:31:28 +0200

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
+import Form from './components/Form/Form';
 import './index.css';
 import { twitterAccounts } from './data/twitterAccounts';
 
 const App = () => {
   const [items, setItems] = useState(twitterAccounts);
 
+  const addItem = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
       <ListWrapper items={items} />
+      <Form submitFn={addItem} />
     </>
   );
 }
