cd4a722ccc9776f31f6711df37dd01e12b5d109a
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 13:43:13 +0200

    Change prop name to proper one, fix proptypes

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ListItem.css';
 
-const ListItem = ({ name, description, image, link }) => (
+const ListItem = ({ name, description, image, twitterLink }) => (
   <li className="listItem__wrapper">
     <img className="listItem__image" src={image} alt={name} />
     <div>
       <h2 className="listItem__name">{name}</h2>
       <p className="listItem__description">{description}</p>
       <a
-        href={link}
+        href={twitterLink}
         className="listItem__button"
         target="_blank"
         rel="noopener noreferrer"
@@ -24,11 +24,11 @@ ListItem.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
   image: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired
+  twitterLink: PropTypes.string.isRequired
 }
 
 ListItem.defaultProps = {
-  description: 'One of the React creators'
+  description: 'One of the React creator'
 }
 
 export default ListItem;
