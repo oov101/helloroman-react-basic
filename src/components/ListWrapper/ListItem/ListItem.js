948adf22970fcdb4804aeee222c4cc46a41c873f
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 11:55:47 +0200

    Add missing button link, change button to anchor, change image alt to prop name

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './ListItem.css';
 
-const ListItem = ({ name, description, image }) => (
+const ListItem = ({ name, description, image, link }) => (
   <li className="listItem__wrapper">
-    <img className="listItem__image" src={image} alt="Dan Abramov" />
+    <img className="listItem__image" src={image} alt={name} />
     <div>
       <h2 className="listItem__name">{name}</h2>
       <p className="listItem__description">{description}</p>
-      <button className="listItem__button">visit twitter page</button>
+      <a
+        href={link}
+        className="listItem__button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        visit twitter page
+      </a>
     </div>
   </li>
 );
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -9,21 +9,25 @@ const ListWrapper = () => (
       name={twitterAccounts[0].name}
       description={twitterAccounts[0].description}
       image={twitterAccounts[0].image}
+      link={twitterAccounts[0].twitterLink}
     />
     <ListItem
       name={twitterAccounts[1].name}
       description={twitterAccounts[1].description}
       image={twitterAccounts[1].image}
+      link={twitterAccounts[1].twitterLink}
     />
     <ListItem
       name={twitterAccounts[2].name}
       description={twitterAccounts[2].description}
       image={twitterAccounts[2].image}
+      link={twitterAccounts[2].twitterLink}
     />
     <ListItem
       name={twitterAccounts[3].name}
       description={twitterAccounts[3].description}
       image={twitterAccounts[3].image}
+      link={twitterAccounts[3].twitterLink}
     />
   </ul>
 );
