c6287c0b14b80937f6d59d9824d664421c4537e6
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 10:51:44 +0200

    Add ListItem

---
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ListItem = () => <li>item1</li>;
+
+export default ListItem;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import ListItem from './ListItem/ListItem';
 
 const ListWrapper = () => (
   <ul>
-    <li>List Item 1</li>
-    <li>List Item 2</li>
-    <li>List Item 3</li>
+    <ListItem />
   </ul>
 );
 
