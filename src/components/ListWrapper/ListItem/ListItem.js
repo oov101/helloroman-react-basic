b5654de7fb78bae08114c95a343f9e52d43eb923
Dawid Walenciej <dawid101w@gmail.com>
2019-08-13 11:06:41 +0200

    Create HTML structure of ListItem component

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './ListItem.css';
+import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
-const ListItem = () => <li className="listItemWrapper">item1</li>;
+const ListItem = () => (
+  <li className="listItemWrapper">
+    <img src={danAbramovImage} alt="Dan Abramov" />
+    <h2>Dan Abramov</h2>
+    <p>Working on @reactjs. The demo guy.</p>
+    <button>visit twitter page</button>
+  </li>
+);
 
 export default ListItem;
